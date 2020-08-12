b69d50a02dd90dbed74d4e462e84989ac6d582f7
dmmaia <david_mmaial.hotmail.com>
2020-08-12 13:22:16 -0300

    getting instagram url imgs

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const { throws } = require('assert');
 
 (async() => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
-    await page.goto('https://www.google.com.br/');
-    await page.screenshot({ path: 'screenshot.png'});
+    await page.goto('https://www.instagram.com/sordavidmaia');
     
+    const imgList = await page.evaluate(()=>{
+        const nodeList = document.querySelectorAll('article img');
+
+        const imgArray = [...nodeList];
+
+        const imgList = imgArray.map( ({src}) => ({
+            src
+        }))
+
+        return imgList;
+    });
+
+    fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err =>{
+        if(err) throw new Error('Deu errado aqui, fera');
+
+        console.log('Nothing to report')
+    })
 
     await browser.close();
 })();
